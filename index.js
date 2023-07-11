6964a369b93c352d019139e1c088faa87ff4755f
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-07-11 14:38:49 +0900

    属性値の書き換え

--- index.js
+++ index.js
@@ -12,8 +12,32 @@ class MyElement extends HTMLElement {
     this.setAttribute('foo', val);
   }
 
+  //監視する属性を指定
+  static get observedAttributes() {
+    return ['foo'];
+  }
+
+  //上記で指定した属性が変更された際に呼び出される
+  attributeChangedCallback(attr, oldValue, newValue) {
+    //foo 属性が変更された場合（第1引数の属性名が foo の場合）
+    if (attr === 'foo') {
+      //値が同じであれば何もしない
+      if (oldValue === newValue) return;
+      //foo 属性（プロパティ）の値を更新（この場合、getter/setter があるので省略可能）
+      this[attr] = newValue;
+      //出力を更新
+      this.textContent = `Hello ${this.foo}!`;
+    }
+  }
+
   connectedCallback() {
-    this.textContent = `${ this.foo }`;
+    if(this.foo) {
+      //foo 属性の値があればその値を使って内容を更新
+      this.textContent = `Hello ${ this.foo }!`;
+    }else{
+      //foo 属性の値がなければ「Hello Foo!」と表示
+      this.textContent = 'Hello Foo!';
+    }
   }
 }
 customElements.define('my-element', MyElement);
